1ef0fbfd845a232e4429ca03cb98912683c8ec4d
islam.walied <islam.walied96@gmail.com>
2025-10-10 14:21:41 +0300

    remove / route

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -39,13 +39,12 @@
                 const input = event.target;
                 const preview = document.getElementById('preview');
 
-                // التحقق من وجود صورة تم اختيارها
                 if (input.files && input.files[0]) {
                     const reader = new FileReader();
 
                     reader.onload = function(e) {
                         preview.src = e.target.result;
-                        preview.style.display = 'block'; // إظهار الصورة
+                        preview.style.display = 'block';
                     };
 
                     reader.readAsDataURL(input.files[0]);

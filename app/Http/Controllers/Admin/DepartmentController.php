128e57934ecff1a7951ed9f1a38b9c64baad808f
islam.walied <islam.walied96@gmail.com>
2025-10-14 02:44:51 +0300

    make department section

---
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreDepartmentRequest;
+use App\Http\Requests\UpdateDepartmentRequest;
+use App\Models\Department;
+
+class DepartmentController extends Controller
+{
+
+    public function index()
+    {
+        $departments = Department::with('person')->paginate(10);
+        return view('admin.departments.index', compact('departments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+//        return view('admin.departments.create' , compact('product' , 'categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDepartmentRequest $request)
+    {
+        $department = [
+            'name' => $request->name,
+        ];
+        Department::create($department);
+
+        return redirect()->route('admin.departments.index')->with('success', 'Department created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Department $department)
+    {
+        return view('admin.departments.show', compact('department'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Department $department)
+    {
+//        return view('admin.departments.edit' , compact('product' , 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDepartmentRequest $request, Department $department)
+    {
+        $department->name = $request->name ?? $department->name;
+        $department->save();
+
+        return redirect()->route('admin.departments.index')->with('success', 'Department updated successfully');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Department $department)
+    {
+        $department->delete();
+        return redirect()->route('admin.departments.index')->with('success', 'Department deleted successfully');
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DepartmentController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProductImageController;
 use Illuminate\Support\Facades\Route;
@@ -28,5 +29,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
 //    });
     Route::resource('categories', CategoryController::class);
     Route::resource('products', ProductController::class);
+    Route::resource('departments', DepartmentController::class);
     Route::resource('product-images', ProductImageController::class);
 });
